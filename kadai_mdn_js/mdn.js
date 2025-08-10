678cfc87b5de13381211f1495444e2a444a633cd
KahoNakanishi <kahomaru0621@gmail.com>
2025-08-10 13:33:27 +0900

    Add files via upload

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,10 @@
+const today = new Date();
+
+const year = today.getFullYear();     
+const month = today.getMonth() + 1;    
+const day = today.getDate();           
+
+const dateString = `${year}年${month}月${day}日`;
+
+console.log(dateString)
+
